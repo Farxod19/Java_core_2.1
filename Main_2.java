3a51c8e7f5db45ef68528c035256fdacfec2add5
Farxod19 <farxodmagdiyev19@gmail.com>
2026-01-29 23:32:00 +0500

    Update Main_2.java

--- Main_2.java
+++ Main_2.java
@@ -1,18 +1,17 @@
 import java.util.Scanner;
 
-public class Main_2 {
+public class Taks2 {
     public static void main(String[] args){
         Scanner few=new Scanner(System.in);
 
         System.out.print("Radiusni kiriting: ");
         double R=few.nextDouble();
 
-        if(R<0){
-            System.out.print("Radius musbat son bolishi kerak");
-        }
-        else if(R >= 0){
-            double S=Math.PI*(R*R);
-            System.out.print("Javob: " + S);
+        if(R<=0){
+            System.out.print("Error! ");
+        }else{
+            double S=Math.PI*R*R;
+            System.out.print("Natija: " + S);
         }
 
     }
